c40fcc80ffa704861b1ab2c8314d1e8c1bd2d8b4
Victor Jouhoff <jouhoff@pm.me>
2023-07-15 17:35:28 +0200

    filmography update

---
+++ 01_testing.py
@@ -0,0 +1,80 @@
+from tmdbv3api import TMDb, Person, Movie
+import csv
+import argparse
+
+# Create an instance of TMDb
+tmdb = TMDb()
+
+# Function to read the API key from .secrets/rootkey.csv
+def read_api_key():
+    with open('.secrets/rootkey3.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            return row[0]  # Assuming the API key is in the first column (modify as per your CSV structure)
+
+# Set the API key
+tmdb.api_key = read_api_key()
+
+# Function to get the filmography details for a person
+def get_person_filmography(person_id):
+    person = Person()
+    filmography = person.movie_credits(person_id).cast
+    return filmography
+
+# Function to get the genre names for a movie ID
+def get_movie_genres(movie_id):
+    movie = Movie()
+    movie_details = movie.details(movie_id)
+    genres = movie_details.genres
+    return genres
+
+# Function to add the film IDs, names, release years, and genres to columns 5, 6, and additional columns
+def add_filmography_details(person_id):
+    filmography = get_person_filmography(person_id)
+
+    # Collect unique genre names from the filmography
+    unique_genres = set()
+    for film in filmography:
+        movie_id = film['id']
+        genres = get_movie_genres(movie_id)
+        for genre in genres:
+            unique_genres.add(genre['name'])
+
+    # Prepare CSV header
+    header = ['Column 5', 'Column 6', 'Column 7']
+    genre_columns = list(unique_genres)
+    header.extend(genre_columns)
+
+    # Prepare data rows
+    rows = []
+    for film in filmography:
+        movie_id = film['id']
+        film_name = film['title']
+        release_year = film['release_date'].split('-')[0] if film['release_date'] else 'N/A'
+        genres = get_movie_genres(movie_id)
+
+        row = [movie_id, film_name, release_year]
+        for genre_column in genre_columns:
+            if any(genre['name'] == genre_column for genre in genres):
+                row.append('X')
+            else:
+                row.append('')
+
+        rows.append(row)
+
+    # Write the filmography details to a CSV file
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(header)  # Write header row
+        writer.writerows(rows)  # Write data rows
+
+    print("Filmography details have been written to output.csv")
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Script to retrieve filmography details and genres for a person")
+parser.add_argument("--genre", action="store_true", help="Include genres in output")
+args = parser.parse_args()
+
+# Example usage
+person_id = input("Enter the person ID: ")
+add_filmography_details(person_id)
