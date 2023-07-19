ce5d2ebff2b07a12cadb59b7a2f0cc0715ca2d81
Victor Jouhoff <jouhoff@pm.me>
2023-07-19 19:00:11 +0200

    Update changes

--- up.sh
+++ up.sh
@@ -7,5 +7,22 @@ git add .
 commit_message="Update changes"
 git commit -m "$commit_message"
 
-# Push changes to the master branch on the origin remote
-git push origin master
+# Display the action and filename for each file
+git_status=$(git status --porcelain)
+while IFS= read -r line; do
+    action=$(echo "$line" | awk '{print $1}')
+    filename=$(echo "$line" | awk '{print $2}')
+    echo "$action -- $filename"
+done <<< "$git_status"
+
+# Prompt for confirmation to push
+read -p "Do you want to push to the master branch? (y/n) [default: y]: " choice
+choice=${choice:-y}  # Set default value to 'y' if no input is provided
+
+# Check the user's choice
+if [[ $choice == "y" || $choice == "Y" ]]; then
+    # Push changes to the master branch on the origin remote
+    git push origin master
+else
+    echo "Aborted. Changes were not pushed to the master branch."
+fi
