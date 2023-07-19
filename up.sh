242d978eba2aa9465d2a2ce0bd38d5f5886fb82d
Victor Jouhoff <jouhoff@pm.me>
2023-07-19 19:04:46 +0200

    Update changes

--- up.sh
+++ up.sh
@@ -7,35 +7,13 @@ git add .
 commit_message="Update changes"
 git commit -m "$commit_message"
 
-# Display the changes
-git_diff=$(git diff --numstat)
-changes=()
+# Display the action and filename for each file
+git_status=$(git status --porcelain)
 while IFS= read -r line; do
-    added=$(echo "$line" | awk '{print $1}')
-    deleted=$(echo "$line" | awk '{print $2}')
-    filename=$(echo "$line" | awk '{print $3}')
-    
-    if [[ $added != 0 && $deleted != 0 ]]; then
-        change="changed"
-    elif [[ $added != 0 ]]; then
-        change="insertions"
-    elif [[ $deleted != 0 ]]; then
-        change="removals"
-    fi
-
-    changes+=("$change: \"$filename\"")
-done <<< "$git_diff"
-
-# Prepare and display the changes summary
-echo "---"
-if [[ ${#changes[@]} -eq 1 ]]; then
-    echo "${changes[0]}"
-else
-    echo "${#changes[@]} files changed"
-    for change in "${changes[@]}"; do
-        echo "- $change"
-    done
-fi
+    action=$(echo "$line" | awk '{print $1}')
+    filename=$(echo "$line" | awk '{print $2}')
+    echo "$action -- $filename"
+done <<< "$git_status"
 
 # Prompt for confirmation to push
 read -p "Do you want to push to the master branch? (y/n) [default: y]: " choice
